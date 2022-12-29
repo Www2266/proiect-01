d4dfd6a7d96086d11c4a4d7df540b3099aa8cb80
Teffiry <wkwwlw@163.com>
2022-12-29 20:32:16 +0800

    注释内容

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2022-12-29 20:03:07
+ * @LastEditTime: 2022-12-29 20:03:08
+ * @LastEditors: DESKTOP-9CH70BE
+ * @Description: In User Settings Edit
+ * @FilePath: \前端学习\project-01\index.js
+ */
